ce5951b2ebd4c060dd7a23203797155c9eb60df5
FishFinsMertz <thn.anbui@gmail.com>
2025-01-13 21:12:42 -0500

    Player movement Implementation
    Double jump included

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
+    private float fallMultiplier = 2.5f; 
+    private bool isFacingRight = true;
+
+    private int doubleJumpCounter;
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doubleJumpCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonDown("Jump") && !IsGrounded() && doubleJumpCounter < 1) {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            doubleJumpCounter++;
+        }
+
+
+        // Holding jump = jump higher
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+
+        // Apply stronger gravity when falling
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+        if (grounded) {
+            doubleJumpCounter = 0; // Reset jump counter when grounded
+        }
+
+        return grounded;
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+}
